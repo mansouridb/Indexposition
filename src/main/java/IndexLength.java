88d43d0d411aeb1ab4b9877cd658f30c18f77497
Majid <Mansouridb@gmail.com>
2021-04-21 11:16:10 +0200

    Initial commit

---
+++ src/main/java/IndexLength.java
@@ -0,0 +1,8 @@
+public class IndexLength {
+    public static void main(String[] args) {
+        String str ="Long example sentence";
+        int index=str.charAt(6);
+        System.out.println("character index 6=  " + (char)(index));
+                
+    }
+}
